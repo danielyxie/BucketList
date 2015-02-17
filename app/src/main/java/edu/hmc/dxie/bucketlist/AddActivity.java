202549f04f9d6775e07dd59e7527ef4d78458197
JustisAllen <JustisAllen@me.com>
2015-02-17 00:41:49 -0800

    Made it so that the Done and Return buttons simulate clicking the confirm add button

--- app/src/main/java/edu/hmc/dxie/bucketlist/AddActivity.java
+++ app/src/main/java/edu/hmc/dxie/bucketlist/AddActivity.java
@@ -3,23 +3,45 @@ package edu.hmc.dxie.bucketlist;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by justisallen and kaitlynanderson on 2/15/15.
  */
 public class AddActivity extends ActionBarActivity implements View.OnClickListener{
     
+    EditText addEditText;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
-        
+
         Button addButton = (Button) findViewById(R.id.button_confirm_add);
         addButton.setOnClickListener(this);
+        
+        addEditText = (EditText) findViewById(R.id.text_add);
+
+        // Clicks the add Button when the Done or Return buttons on the keyboard are pressed
+        addEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if ( (event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) ||
+                        (actionId == EditorInfo.IME_ACTION_DONE)) {
+                    onClick(v);
+                }
+                return false;
+            }
+        });
+        
+        
     }
 
     @Override
@@ -41,9 +63,6 @@ public class AddActivity extends ActionBarActivity implements View.OnClickListen
     public void onClick(View v) {
 
         // Get the text from the EditText
-        EditText addEditText = (EditText) findViewById(R.id.text_add);
-        
-        // Take what was typed into the EditText
         String itemText = addEditText.getText().toString();
 
         Intent addedItem = new Intent();
